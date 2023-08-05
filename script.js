fee1b89c2b9697d52978d3b8f9f9ce64d31b0824
TROY PAOLO ESPIEL <tomespiel7@gmail.com>
2023-08-05 22:26:26 +0800

    task1completed

---
+++ script.js
@@ -0,0 +1,33 @@
+// script.js
+
+function sendInquiry() {
+  // Get form data
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const contactNumber = document.getElementById("contactNumber").value;
+  // Get other form data in a similar manner (e.g., service type, etc.)
+  const message = document.getElementById("message").value;
+
+  // Build the message to be sent in the chat window
+  const inquiryMessage = `Inquiry from ${firstName} ${lastName}\nEmail: ${email}\nContact Number: ${contactNumber}\nMessage: ${message}`;
+
+  // Create the Facebook Messenger link with the pre-filled message
+  const fbPageUsername = "https://m.me/callmEsmayl"; // Replace with your Facebook Page username or Page ID
+  const messengerLink = `https://www.facebook.com/callmEsmayl?mibextid=ZbWKwL${fbPageUsername}?ref=${encodeURIComponent(
+    inquiryMessage
+  )}`;
+
+  // Redirect the user to the Facebook Messenger link
+  window.location.href = messengerLink;
+}
+
+function resetForm() {
+  // Reset the form elements to their initial state
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("contactNumber").value = "";
+  // Reset other form elements in a similar manner (e.g., service type, etc.)
+  document.getElementById("message").value = "";
+}
